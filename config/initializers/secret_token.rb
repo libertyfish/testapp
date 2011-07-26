b7d14cbbfc663827d794e4a3c9481a08a5376891
hogehoge <h_hakusui@libertyfish.co.jp>
2011-07-26 17:11:33 +0900

    1st commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bundletest2::Application.config.secret_token = 'ca557f046daae648b5cde4088efa5480cea6b086a4843013fb049cf282de7c0050e10ef7be5df430e0fd313da1112cc725eba8e7f8f1a5912ce72fb30164ce39'
